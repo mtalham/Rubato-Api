ebe8a8c363fe44b5d92c927cc27023979e50e31d
Talha <mtalhamunir@outlook.com>
2019-09-22 20:06:27 +0200

    backend for rubato

---
+++ src/main/java/no/rubato/model/Audio.java
@@ -0,0 +1,63 @@
+package no.rubato.model;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Audio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_audio")
+    private long idAudio;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "path")
+    private String path;
+    @Column(name = "type")
+    private String type;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Persons persons;
+
+    public long getIdAudio() {
+        return idAudio;
+    }
+
+    public void setIdAudio(int idAudio) {
+        this.idAudio = idAudio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Persons getPersons() {
+        return persons;
+    }
+
+    public void setPersons(Persons persons) {
+        this.persons = persons;
+    }
+}
