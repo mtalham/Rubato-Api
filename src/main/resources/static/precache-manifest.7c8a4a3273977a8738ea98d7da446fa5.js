107373fffefc3da7fa5fd12f2cf6cced47cfdc43
Talha Munir <46424469+mtalham@users.noreply.github.com>
2020-03-28 21:09:21 +0100

    minor changes

---
+++ src/main/resources/static/precache-manifest.7c8a4a3273977a8738ea98d7da446fa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c0f2650d294ba89b57b1d6b9cb226e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "900e5ad6f04644a47b38",
+    "url": "/static/css/main.c38a6c91.chunk.css"
+  },
+  {
+    "revision": "2d85c52b0e4f71b86b72",
+    "url": "/static/js/2.ae646453.chunk.js"
+  },
+  {
+    "revision": "900e5ad6f04644a47b38",
+    "url": "/static/js/main.d7e91ac5.chunk.js"
+  },
+  {
+    "revision": "6cd46acd0019f0d61638",
+    "url": "/static/js/runtime~main.60047510.js"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e04085e60c8cf3704ecd492be029e47.js"
+  "/precache-manifest.7c8a4a3273977a8738ea98d7da446fa5.js"
 );
 
 self.addEventListener('message', (event) => {
